d9f3e3ea0337d173f37d113bf19903971b17d11e
rishab99058 <rishab99058@gmail.com>
2025-09-21 13:56:22 +0530

    first commit

---
+++ backend/vingo/src/main/java/com/rishab99058/vingo/VingoApplication.java
@@ -0,0 +1,13 @@
+package com.rishab99058.vingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VingoApplication.class, args);
+	}
+
+}
---
+++ backend/vingo/src/test/java/com/rishab99058/vingo/VingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishab99058.vingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
